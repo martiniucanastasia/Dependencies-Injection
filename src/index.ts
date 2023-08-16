a6d08e81354cb670b8fde663961795ec246664f0
amartiniuc <c_amartiniuc@costco.com>
2023-08-16 18:05:01 +0300

    add solution

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import IoCContainer from 'ioc-lite';
+import { createIoCContainer } from './ioc';
+
 import { Users } from './services/users';
 import { Logger } from './services/logger';
 
-import type { User, ApiConfig } from './types';
+import type { User, ApiConfig, IoCContainer as IoCType } from './types';
+
+let ioc: IoCContainer<IoCType> = createIoCContainer();
 
 const renderUsers = async (config: ApiConfig) => {
-  const usersService = new Users(config);
+  const usersService: Users = ioc.resolve('users');
   const users = await usersService.getUsers();
-
   const listNode = document.getElementById('users-list');
 
   users.forEach((user: User) => {
@@ -19,13 +23,16 @@ const renderUsers = async (config: ApiConfig) => {
 
 const app = () => {
   const config = (window as any).__CONFIG__;
+
   delete (window as any).__CONFIG__;
+
+  ioc.register('apiConfig', config.api);
 
   renderUsers(config.api);
 };
 
 window.onload = (event: Event) => {
-  const logger = new Logger();
+  const logger: Logger = ioc.resolve('logger');
 
   logger.info('Page is loaded.');
 
--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,16 @@
 import IoCContainer from 'ioc-lite';
+import { IoCContainer as IoCType } from './../types/index';
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
+import { Logger } from '../services/logger';
+import { HTTP } from '../services/http';
+import { Users } from '../services/users';
 
-export const createIoCContainer = () =>  {
-  const ioc = new IoCContainer();
-  // you can register some resources right now below...
+export const createIoCContainer = () => {
+  const ioc = new IoCContainer<IoCType>();
 
+  ioc.registerClass('logger', Logger);
+  ioc.registerClass('http', HTTP);
+  ioc.registerClass('users', Users);
+  
   return ioc;
 };
